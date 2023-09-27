46a47748ed2d0863aab71dd2e5b5bc40c3e69136
g-faraco1 <g.faraco1@studenti.unisa.it>
2023-09-27 18:29:20 +0200

    Sottrazione e Divisione aggiunte nella classe Calcolatrice con relativi Test Case

--- src/main/java/Calcolatrice.java
+++ src/main/java/Calcolatrice.java
@@ -13,4 +13,17 @@ public class Calcolatrice {
     public int moltiplicazione(int a, int b){
         return a * b;
     }
+
+    public int sottrazione(int a, int b){
+        return a-b;
+    }
+
+    public double divisione(int a, int b){
+        if(b!=0){
+            return a/b;
+        }
+        else return 0;
+    }
+
+
 }
